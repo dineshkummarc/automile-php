0e58d855a32a9f6b449d19cd16aa116990bec49f
Dinesh Kumar <mapmydestination@gmail.com>
2025-04-29 20:36:28 +0530

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automile/automile-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0828bc9027e1b612de135ae3d2d81dcfbd1914aa',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'automile/automile-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0828bc9027e1b612de135ae3d2d81dcfbd1914aa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
